5ce56487385bb3cd2c256137f2b4227b8808611f
Eleanor Byass <eleanorbyass@gmail.com>
2020-05-29 11:53:58 +0100

    deploying final bits

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
-const port = 8000
-const dbURI = 'mongodb://localhost/accountable-db-2'
-const secret = 'accountable' 
+const port = process.env.PORT || 8000
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/accountable-db-2'
+const secret = process.env.SECRET || 'accountable' 
 
 module.exports = {
   port,
--- index.js
+++ index.js
@@ -10,9 +10,13 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
     if (err) return console.log(err)
     console.log('Mongo is Connected!')
   })
+	
+app.use(express.static(`${__dirname}/frontend/build`))
 
 app.use(bodyParser.json())
 
 app.use('/api', router)
+
+app.use('/*', (req, res) => res.sendFile(`${__dirname}/frontend/build/index.html`)) //* any other route that deosn't deliver to /api will direct them to the index.html frontend page which has been made available from line 14 
 
 app.listen(port, () => console.log(`Up and running on port ${port}`))
